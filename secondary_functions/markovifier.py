fd64f228b92554147e47cfeda64a32056e5de3a1
TactiCool-HUN <108133191+TactiCool-HUN@users.noreply.github.com>
2023-10-18 00:45:44 +0300

    markov learner fix2

--- secondary_functions/markovifier.py
+++ secondary_functions/markovifier.py
@@ -3,7 +3,8 @@ import pandas as pd
 from itertools import chain
 
 
-def markov_learner(text):
+def markov_learner(text: str):
+    text = text.replace("@Dice God", "")
     with open("data_holder/markov_studies.txt", "a") as f:
         f.write(text)
 
