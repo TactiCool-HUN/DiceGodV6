86b550805f04040142f64ec92e371c203e8cb80a
TactiCool-HUN <108133191+TactiCool-HUN@users.noreply.github.com>
2023-10-18 02:11:28 +0300

    markovifier spaces

--- secondary_functions/markovifier.py
+++ secondary_functions/markovifier.py
@@ -5,13 +5,14 @@ import random
 def markov_learner(text: str):
     text = text.replace("@Dice God ", "")
     with open("data_holder/markov_studies.txt", "a") as f:
-        f.write(text)
+        f.write(f"{text}\n")
 
 
 # noinspection SpellCheckingInspection
 def markovifier():
     with open("data_holder/markov_studies.txt", "r") as f:
-        text = f.read()
+        text = f.readlines()
+    text = " ".join(text)
 
     markov_chain_model = markovify.Text(text)
     return markov_chain_model.make_short_sentence(70, 20, tries = 20)
