3e0038cee8dbbec78030ba5a30def00be4e9ae84
Tacti <domokose99@gmail.com>
2024-10-08 23:01:00 +0300

    markovifier fix1

--- secondary_functions/markovifier.py
+++ secondary_functions/markovifier.py
@@ -2,16 +2,20 @@ import markovify
 
 
 def markov_learner(text: str, guild: int):
-    text = text.replace("<@953258800759070720> ", "")
-    with open(f"data_holder/markov_studies/{guild}.txt", "a") as f:
-        f.write(f"{text}\n")
+	text = text.replace("<@953258800759070720> ", "")
+	with open(f"data_holder/markov_studies/{guild}.txt", "a") as f:
+		f.write(f"{text}\n")
 
 
 # noinspection SpellCheckingInspection
 def markovifier(guild: int):
-    with open(f"data_holder/markov_studies/{guild}.txt", "r") as f:
-        text = f.readlines()
-    text = "\n".join(text)
+	try:
+		with open(f"data_holder/markov_studies/{guild}.txt", "r") as f:
+			text = f.readlines()
+	except FileNotFoundError:
+		return
 
-    markov_chain_model = markovify.Text(text)
-    return markov_chain_model.make_short_sentence(50, 20, tries = 20)
+	text = "\n".join(text)
+
+	markov_chain_model = markovify.Text(text)
+	return markov_chain_model.make_short_sentence(50, 20, tries = 20)
